6eb85fa1947d23b723b077ff9532de15c10e6f42
Дмитрий Лохов <DLokhov@me.com>
2020-05-06 12:31:52 +0900

    2

--- lesson-3.py
+++ lesson-3.py
@@ -1,7 +1,13 @@
 # 1 методами строк очистить текст от знаков препинания;
 #################
-print()
-str = '!!!Погружение в IT-профессию!!! подразумевает постоянный контакт с изучаемыми технологиями, выполнение практических заданий и общение с наставником. Для этого мы создали собственную среду обучения.'
+
+import pymorphy2
+morph = pymorphy2.MorphAnalyzer()
+
+tfile = open('text.txt',encoding='utf-8')
+text=tfile.read()
+
+str = 'text'
 
 # str = str.replace(',', '').replace('.', '').replace(';','').replace('—','').replace('«','').replace('»','').replace('?','').replace('!','').replace('(','').replace(')','')
 # print(str)
@@ -36,23 +42,24 @@ print(answer4)
 # 5 вывести 5 наиболее часто встречающихся слов (sort), вывести количество разных слов в тексте (set).
 #################
 
-list_answer4 = list(answer4.items())
-list_answer4.sort(key=lambda i: i[1])
-list_answer4.reverse()
-count = 0
-often_words = ''
-for i in list_answer4:
-    if count != 5:
-        count += 1
-        often_words += i[0] + ', '
-print('5 наиболее часто встречающихся слов: ', often_words)
+text_list=str.split()
+print('Создаём List')
+print(text_list)
+# Приводим к нижнему регистру
+text_list=list(map(lambda x: x.lower(),text_list))
+print('Приводим к нижнему регистру')
+print(text_list)
+# Получаем нормальную форму
+text_list=list(map(lambda x: morph.parse(x)[0].normal_form,text_list))
+print('Получаем нормальную форму')
+print(text_list)
+text_dict={a:text_list.count(a) for a in text_list}
+print('Создание словаря СЛОВО:ВСТРЕЧАЕТСЯ')
+print(text_dict)
 
-set_one = set(answer3)
-set_second = set(answer3)
-set_third = set_one.intersection(set_second)
-count = 0
-for i in set_third:
-    count += 1
-print('Количество разных слов: ', count)
+sort_list=list(text_dict.items())
+sort_list.sort(key=lambda i: i[1],reverse=True)
+print('5 наиболее встречающихся')
+print(sort_list[:5])
 #################
 
